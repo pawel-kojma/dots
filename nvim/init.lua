7085c1a1a37647e88043bd23b0a4beb7d0451a80
pawel-kojma <pawel.kojma@hotmail.com>
2024-09-17 13:15:51 -0500

    nvim

---
+++ nvim/init.lua
@@ -0,0 +1,22 @@
+-- Line numbers
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+-- Searching
+vim.opt.hlsearch = false
+
+-- Tabs
+vim.opt.expandtab = true
+vim.opt.tabstop = 4
+vim.opt.wrap = false
+
+-- Scroll
+vim.opt.scrolloff = 999
+
+-- Cliboard
+vim.opt.clipboard = "unnamedplus"
+
+-- Splitting
+vim.opt.splitbelow = true
+vim.opt.splitright = true
+
