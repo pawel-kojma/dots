8c237ecb1640fd12bf72caa2bb7814a4cca8dd58
pawel-kojma <pawel.kojma@hotmail.com>
2024-09-20 15:20:53 -0500

    add lazy

--- nvim/init.lua
+++ nvim/init.lua
@@ -8,6 +8,8 @@ vim.opt.hlsearch = false
 -- Tabs
 vim.opt.expandtab = true
 vim.opt.tabstop = 4
+
+-- Line wrap
 vim.opt.wrap = false
 
 -- Scroll
@@ -19,4 +21,28 @@ vim.opt.clipboard = "unnamedplus"
 -- Splitting
 vim.opt.splitbelow = true
 vim.opt.splitright = true
+
+-- Colors
+vim.opt.termguicolors = true
+
+-- Edit
+vim.opt.virtualedit = "block"
+
+-- Preview & Commands
+vim.opt.inccommand = "split"
+vim.opt.ignorecase = true
+
+-- Bootstrap lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+end
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup({
+    "EdenEast/nightfox.nvim"
+})
+
+vim.cmd.colorscheme("carbonfox")
 
