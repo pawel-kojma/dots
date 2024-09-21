6ddb961d40119bba21efffa9d2cd81e7e5104be7
pawel-kojma <pawel.kojma@hotmail.com>
2024-09-21 13:04:37 -0500

    clean nvim config

---
+++ nvim/lua/options.lua
@@ -0,0 +1,34 @@
+-- Line numbers
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+-- Searching
+vim.opt.hlsearch = false
+
+-- Tabs
+vim.opt.expandtab = true
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+
+-- Line wrap
+vim.opt.wrap = false
+
+-- Scroll
+vim.opt.scrolloff = 999
+
+-- Cliboard
+vim.opt.clipboard = "unnamedplus"
+
+-- Splitting
+vim.opt.splitbelow = true
+vim.opt.splitright = true
+
+-- Colors
+vim.opt.termguicolors = true
+
+-- Edit
+vim.opt.virtualedit = "block"
+
+-- Preview & Commands
+vim.opt.inccommand = "split"
+vim.opt.ignorecase = true
