ef05b6f7256870b1e4319cb5283faaecd3e45119
pawel-kojma <pawel.kojma@hotmail.com>
2024-09-22 10:08:32 -0500

    nvim progress

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -12,6 +12,26 @@ require("lazy").setup({
         config = function()
             vim.cmd.colorscheme("carbonfox")
         end,
-    }
+    },
+    {
+        "nvim-treesitter/nvim-treesitter",
+        config = function()
+            require("nvim-treesitter.configs").setup({
+                ensure_installed = {"c","lua","vim","vimdoc","query","ocaml"},
+                highlight = {
+                    enable = true,
+                },
+                incremental_selection = {
+                    enable = true,
+                    keymaps = {
+                        init_selection = "<Leader>ss",
+                        node_incremental = "<Leader>si",
+                        scope_incremental = "<Leader>sc",
+                        node_decremental = "<Leader>sd",
+                    },
+                },
+            })
+        end,
+    },
 })
 
