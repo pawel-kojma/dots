9287209a0ac10a3c6d148e205b96e9de896b43f7
pawel-kojma <pawel.kojma@hotmail.com>
2024-09-30 15:06:56 -0500

    add telescope

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -45,13 +45,23 @@ require("lazy").setup({
     },
     {
         "williamboman/mason-lspconfig.nvim",
-        dependencies = {"mason.nvim", "nvim-lspconfig"},
+        dependencies = { "mason.nvim", "nvim-lspconfig" },
         config = function()
             require("mason-lspconfig").setup_handlers({
                 function(server_name)
                     require("lspconfig")[server_name].setup({})
                 end,
             })
+        end,
+    },
+    {
+        "nvim-telescope/telescope.nvim",
+        tag = "0.1.8",
+        dependencies = { "nvim-lua/plenary.nvim" },
+        config = function()
+            local builtin = require("telescope.builtin")
+            vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Telescope find files" })
+            vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Telescope live grep" })
         end,
     },
 })
