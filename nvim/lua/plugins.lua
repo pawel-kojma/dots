953d8f8b68b651f560bb8e84e6b887fef9dbf1e9
pawel-kojma <pawel.kojma@hotmail.com>
2025-02-15 15:17:23 +0100

    lsp definitions for neovim config

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -75,6 +75,17 @@ require("lazy").setup({
             override_del = true,
         },
         lazy = false,
-    }
+    },
+    {
+        "folke/lazydev.nvim",
+        ft = "lua", -- only load on lua files
+        opts = {
+            library = {
+                -- See the configuration section for more details
+                -- Load luvit types when the `vim.uv` word is found
+                { path = "${3rd}/luv/library", words = { "vim%.uv" } },
+            },
+        },
+    },
 })
 
