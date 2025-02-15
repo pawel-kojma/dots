11d40736f524b2c1df3bc16d96cf3846f5921bf5
pawel-kojma <pawel.kojma@hotmail.com>
2025-02-15 19:27:22 +0100

    keymap refa

--- nvim/lua/keymaps.lua
+++ nvim/lua/keymaps.lua
@@ -2,15 +2,17 @@ vim.g.mapleader = " "
 vim.opt.signcolumn = "no"
 
 -- Tabs
-vim.keymap.set({'n','v'}, "<leader>1", function() vim.cmd("tabn1") end)
-vim.keymap.set({'n','v'}, "<leader>2", function() vim.cmd("tabn2") end)
-vim.keymap.set({'n','v'}, "<leader>3", function() vim.cmd("tabn3") end)
-vim.keymap.set({'n','v'}, "<leader>4", function() vim.cmd("tabn4") end)
-vim.keymap.set({'n','v'}, "<leader>5", function() vim.cmd("tabn5") end)
-vim.keymap.set({'n','v'}, "<leader>6", function() vim.cmd("tabn6") end)
-vim.keymap.set({'n','v'}, "<leader>7", function() vim.cmd("tabn7") end)
-vim.keymap.set({'n','v'}, "<leader>8", function() vim.cmd("tabn8") end)
-vim.keymap.set({'n','v'}, "<leader>9", function() vim.cmd("tabn9") end)
-vim.keymap.set({'n','v'}, "<leader>0", function() vim.cmd("tabn10") end)
-vim.keymap.set({'n','v'}, "<leader>n", function() vim.cmd("$tabnew") end)
+vim.keymap.set({ 'n', 'v' }, "<leader>1", function() vim.cmd("tabn1") end)
+vim.keymap.set({ 'n', 'v' }, "<leader>2", function() vim.cmd("tabn2") end)
+vim.keymap.set({ 'n', 'v' }, "<leader>3", function() vim.cmd("tabn3") end)
+vim.keymap.set({ 'n', 'v' }, "<leader>4", function() vim.cmd("tabn4") end)
+vim.keymap.set({ 'n', 'v' }, "<leader>5", function() vim.cmd("tabn5") end)
+vim.keymap.set({ 'n', 'v' }, "<leader>6", function() vim.cmd("tabn6") end)
+vim.keymap.set({ 'n', 'v' }, "<leader>7", function() vim.cmd("tabn7") end)
+vim.keymap.set({ 'n', 'v' }, "<leader>8", function() vim.cmd("tabn8") end)
+vim.keymap.set({ 'n', 'v' }, "<leader>9", function() vim.cmd("tabn9") end)
+vim.keymap.set({ 'n', 'v' }, "<leader>0", function() vim.cmd("tabn10") end)
+vim.keymap.set({ 'n', 'v' }, "<leader>n", function() vim.cmd("$tabnew") end)
 
+-- Oil
+vim.keymap.set("n", "<leader>x", "<CMD>Oil<CR>", { desc = "E[x]plore current directory" })
--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -141,9 +141,11 @@ require("lazy").setup({
         'stevearc/oil.nvim',
         ---@module 'oil'
         ---@type oil.SetupOpts
-        opts = {},
         -- Optional dependencies
         dependencies = { { "echasnovski/mini.icons", opts = {} } },
+        config = function()
+            require("oil").setup({})
+        end,
         -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
         -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
         lazy = false,
