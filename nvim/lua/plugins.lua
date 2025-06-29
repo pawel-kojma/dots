1a3404dc41d3f980da16d4af624df5206f93f130
pawel-kojma <pawel.kojma@hotmail.com>
2025-06-29 21:57:56 +0200

    add lazydev do blink completions

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -216,9 +216,16 @@ require("lazy").setup({
             },
             completion = { documentation = { auto_show = true } },
             sources = {
-                default = { "lsp", "path", "snippets", "buffer" },
+                default = { "lazydev", "lsp", "path", "snippets", "buffer" },
+                providers = {
+                    lazydev = {
+                        name = "LazyDev",
+                        module = "lazydev.integrations.blink",
+                        -- make lazydev completions top priority (see `:h blink.cmp`)
+                        score_offset = 100,
+                    },
+                },
             },
-
             fuzzy = { implementation = "prefer_rust_with_warning" }
         },
         opts_extend = { "sources.default" }
