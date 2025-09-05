a3140c16630761283f99aadab29d1fa94a966d62
pawel-kojma <pawel.kojma@hotmail.com>
2025-09-05 21:12:15 +0200

    diagnostics off and handle paths with spaces

--- nvim/lua/keymaps.lua
+++ nvim/lua/keymaps.lua
@@ -1,5 +1,4 @@
 vim.g.mapleader = " "
-vim.opt.signcolumn = "no"
 
 -- Oil
 vim.keymap.set("n", "<leader>x", "<CMD>Oil<CR>", { desc = "E[x]plore current directory" })
@@ -22,14 +21,37 @@ local tab_map = function(idx)
     end
 end
 
-for i=1,10 do
+for i = 1, 10 do
     tab_map(i)
 end
 vim.keymap.set({ 'n', 'v' }, "<leader>tc", function() vim.cmd("tabclose") end)
 vim.keymap.set({ 'n', 'v' }, "<leader>th", function() vim.cmd("-tabmove") end)
 vim.keymap.set({ 'n', 'v' }, "<leader>tl", function() vim.cmd("+tabmove") end)
 vim.keymap.set({ 'n', 'v' }, "<leader>tn", function()
-    local path = vim.fn.expand("%:h")
-    vim.cmd("tabnew");
-    vim.cmd("Oil " .. path);
+    local t = { path = vim.fn.expand("%:h") }
+    local cmd = string.gsub("Oil '$path'", "%$(%w+)", t)
+    vim.cmd("tabnew")
+    vim.cmd(cmd)
 end)
+
+local is_diagnostics_on = false
+local function toggle_diagnostics()
+    local opts = { -- https://neovim.io/doc/user/diagnostic.html
+        virtual_text = false,
+        signs = false,
+        underline = false,
+    }
+    if is_diagnostics_on then
+        opts.virtual_text = true
+        opts.underline = true
+    else
+        opts.virtual_text = false
+        opts.underline = false
+    end
+    is_diagnostics_on = not is_diagnostics_on
+    vim.diagnostic.config(opts)
+end
+
+-- Diagnostics off by default
+toggle_diagnostics()
+vim.keymap.set({ 'n', 'v' }, "<leader>dt", toggle_diagnostics)
